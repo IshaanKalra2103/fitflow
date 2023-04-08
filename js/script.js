1c55d0ad24fb6925fca2829650b79ebd9252dac4
Ishaan Kalra <74290686+IshaanKalra2103@users.noreply.github.com>
2023-04-08 15:55:03 -0400

    JS implemented
    Js implemented

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+   $(".picture").attr(profile.getImageUrl());
+   $(".name").text(profile.getName());
+   $(".data").css("display","block");
+   $(".sign-in").css("display","none");
   }
+
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      alert("You have been successfully signed out!")
+      $(".sign-in").css("display","block");
+    });
+  }
+
+  
