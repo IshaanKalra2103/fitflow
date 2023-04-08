eba53527d1682b0e5ac2fd80aabd69b8fe2bd8c0
Ishaan Kalra <74290686+IshaanKalra2103@users.noreply.github.com>
2023-04-08 16:05:26 -0400

    new
    new chnages

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-   $(".picture").attr(profile.getImageUrl());
-   $(".name").text(profile.getName());
-   $(".data").css("display","block");
-   $(".sign-in").css("display","none");
+
+  var id_token = googleUser.getAuthResponse().id_token;
+
+  // Redirect the user to the next page
+  window.location.href = "next-page.html";
+  //   var profile = googleUser.getBasicProfile();
+  //  $(".picture").attr(profile.getImageUrl());
+  //  $(".name").text(profile.getName());
+  //  $(".data").css("display","block");
+  //  $(".sign-in").css("display","none");
   }
 
   function signOut() {
