13a98a5f0207c60e78134d8490f5b26fe8309f6c
Ishaan Kalra <74290686+IshaanKalra2103@users.noreply.github.com>
2023-04-09 05:11:41 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ input.addEventListener("keypress", function(event) {
     || input.value.toLowerCase() === "sad" || input.value.toLowerCase() === "depressed" || input.value.toLowerCase() === "down" 
     || input.value.toLowerCase() === "angry" || input.value.toLowerCase() === "frustrated"|| input.value.toLowerCase() === "irritated"){
       emotionalState = input.value.toLowerCase();
-      window.location.href = "http://127.0.0.1:5500/second-screen.html";//Change this.
+      window.location.href = "https://feelfitflow.netlify.app/second-screen.html";//Change this.
     } else{
       alert("Please enter a valid emotional state");
     }
